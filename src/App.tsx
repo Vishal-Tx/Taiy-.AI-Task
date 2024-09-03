8adf1041a2f43cd08c9cb40a18a0668d2e42b98c
Vishal-Tx <vishalguptaa2z111@gmail.com>
2024-09-03 20:47:00 +0530

    add routes-initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Contacts from "./pages/Contacts";
+import Dashboard from "./pages/Dashboard";
+import ContactDetails from "./components/ContactDetails";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Contacts />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/contacts/:id" element={<ContactDetails />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
