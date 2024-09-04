2230e165c19581c0f4039d53a24b69dbccb832aa
Vishal-Tx <vishalguptaa2z111@gmail.com>
2024-09-04 18:07:32 +0530

    update routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Contacts from "./pages/Contacts";
 import Dashboard from "./pages/Dashboard";
 import ContactDetails from "./components/ContactDetails";
+import Layout from "./components/Layout";
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Contacts />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/contacts/:id" element={<ContactDetails />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Contacts />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/contacts/:id" element={<ContactDetails />} />
+          <Route path="/*" element={<Contacts />} />
+
+        </Routes>
+      </Layout>
     </Router>
   );
 };
